39c685912a8807aff43c6985df4db10297ad3ab0
jasstej <73151658+jasstej@users.noreply.github.com>
2024-01-23 10:18:27 +0000

    23-1-2024 c++ lab

--- main.cpp
+++ main.cpp
@@ -1,16 +1,32 @@
-#include<iostream>
+#include <cmath>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-    int i , n , c=0;
-  cout<< "Enter the desired number: ";
-  cin>>n;
-  for(i=1;i<=n;i++)
-    {
-      cout <<i<<"+";
-      c=c+i;
-    }
+int main() {
+  int num, originalNum, remainder, n = 0, result = 0;
 
-    return 0;
+  cout << "Enter a positive integer: ";
+  cin >> num;
+
+  originalNum = num;
+
+  while (originalNum != 0) {
+    originalNum /= 10;
+    ++n;
+  }
+
+  originalNum = num;
+
+  while (originalNum != 0) {
+    remainder = originalNum % 10;
+    result += pow(remainder, n);
+    originalNum /= 10;
+  }
+
+  if (result == num)
+    cout << num << " is an Armstrong number.";
+  else
+    cout << num << " is not an Armstrong number.";
+
+  return 0;
 }
